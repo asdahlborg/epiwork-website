78cbf38f3b1175f7540cf889568c0afb30b517af
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-12 14:45:31 +0100

    Add survey data structure.

---
+++ src/epiweb/apps/survey/data/__init__.py
@@ -0,0 +1,3 @@
+
+from questions import *
+
---
+++ src/epiweb/apps/survey/data/questions.py
@@ -0,0 +1,18 @@
+
+class Survey:
+    def __init__(self):
+        self.sections = []
+
+class Section:
+    def __init__(self, title=None):
+        self.title = title
+        self.questions = []
+
+class Question:
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
+        self.type = 'text'
+        self.length = '250'
+        self.options = []
+
