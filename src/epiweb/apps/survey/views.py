8b1c229aa3891ca80c88f3514d9c7014cf7909fc
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-12 14:48:54 +0100

    Remove form generator from the view.

--- src/epiweb/apps/survey/views.py
+++ src/epiweb/apps/survey/views.py
@@ -7,40 +7,6 @@ from django.http import HttpResponse
 from epiweb.apps.survey import utils
 from epiweb.apps.survey.data import example
 
-def create_field(item):
-    if item['type'] == 'yes-no':
-        field = forms.ChoiceField(widget=forms.RadioSelect,
-                                  choices=[('yes', _('Yes')), ('no', _('No'))])
-
-    elif item['type'] == 'option-multiple':
-        field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                                          choices=zip(range(0, len(item['options'])), item['options']))
-
-    elif item['type'] == 'option-single':
-        field = forms.ChoiceField(widget=forms.RadioSelect,
-                                  choices=zip(range(0, len(item['options'])), item['options']))
-
-    elif item['type'] == 'date':
-        field = forms.DateField(input_formats='%m/%d/%y')
-
-    else:
-        field = forms.CharField()
-
-    field.label = item.get('label', None)
-    field.required = False
-
-    return field
-
-def create_form(data, values=None):
-    if values:
-        f = forms.Form(values)
-    else:
-        f = forms.Form()
-
-    for item in data:
-        f.fields[item['id']] = create_field(item)
-    return f
-
 def index(request):
 
     if request.method == 'POST':
