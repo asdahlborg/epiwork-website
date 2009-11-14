8a1f1a7831bc0ca760d47130e655cd6dc8d8de93
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-14 18:01:25 +0100

    Add untested database insert code.

---
+++ src/epiweb/apps/survey/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Survey(models.Model):
+    created = models.DataTimeField()
+    survey_id = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    definition = models.TextField()
+
+class History(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    survey = models.ForeignKey(Survey)
+    epidb_id = models.CharField(max_length=36, null=True)
+
+class Profile(models.Model)
+    user = models.ForeignKey(User, unique=True)
+    last_survey = models.ForeignKey(History)
+    last_survey_date = models.DateTimeField()
+
+class Unsaved(models.Model)
+    history = models.ForeignKey(History)
+    date = models.DateTimeField()
+    data = models.TextField()
--- src/epiweb/apps/survey/views.py
+++ src/epiweb/apps/survey/views.py
@@ -3,14 +3,49 @@
 from django import forms
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.db import transaction
 
 from epiweb.apps.survey import utils
+from epiweb.apps.survey import models
 from epiweb.apps.survey.data import example
+
+from epidb_client import EpiDBClient
+
+from django.conf import settings
+
+def _create_response_data():
+    return ""
+
+@transaction.commit_on_success
+def _save_survey(request):
+    client = EpiDBClient(settings.EPIDB_API_KEY)
+    client.server = settings.EPIDB_SERVER
+    data = _create_response_data() # TODO
+    res = client.response_submit(data)
+
+    hist = models.History()
+    hist.user = request.user
+    hist.epidb_id = res.get('id', None)
+    hist.survey = request.session['survey_id']
+    hist.save()
+
+    profile = models.Profile.objects.get(user=request.user)
+    profile.last_survey = hist
+    profile.last_survey_date = hist.date
+    profile.save()
+
+    if res.get('id', None) is None:
+        unsaved = models.Unsaved()
+        unsaved.history = hist
+        unsaved.date = hist.date
+        unsaved.data = data
+        unsaved.save()
 
 def index(request):
 
     if request.method == 'POST':
         form = utils.generate_form(example.data.sections[0], request.POST)
+        _save_survey(request) # TODO
     else:
         form = utils.generate_form(example.data.sections[0])
 
