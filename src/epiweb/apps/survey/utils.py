3cd8955aefb675e54d24c8d623f67ab87d71b1c1
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-15 12:45:29 +0100

    Remove the old javascript helper.

--- src/epiweb/apps/survey/utils.py
+++ src/epiweb/apps/survey/utils.py
@@ -7,62 +7,6 @@ from epiweb.apps.survey import definitions as d
 
 _ = lambda x: x
 
-def generate_js_helper(section):
-    obj = 's'
-    lines = []
-    ids = []
-    for question in section.questions:
-        ids.append(question.id)
-        if question.condition is not None:
-            lines.append('%s.eval["%s"] = function() { return %s; }' % (obj, question.id, 
-                            create_js_statement(question.condition)))
-        else:
-            lines.append('%s.eval["%s"] = function() { return true; }' % (obj, question.id))
-
-    return """
-(function() {
-var q = [%(id)s];
-var s = new Survey();
-s.set_questions(q);
-s.eval = [];
-%(condition)s
-s.init();
-})();
-""" % { 
-        'condition': "\n".join(lines), 
-        'id': ", ".join(map(lambda x: "'%s'" % x, ids)) 
-    }
-        
-
-def get_intake_value(id):
-    return "true"
-
-def create_js_statement(v, obj='s'):
-    if isinstance(v, Q):
-        return "%s.Q('%s')" % (obj, v.id)
-    elif isinstance(v, Intake):
-        return get_intake_value(v.id)
-    elif isinstance(v, OneOf):
-        options = map(lambda x: str(x), v.options)
-        return "%s.OneOf(%s, [%s])" % (obj, create_js_statement(v.a), ", ".join(options))
-    elif isinstance(v, NotEmpty):
-        return "%s.NotEmpty(%s)" % (obj, create_js_statement(v.a))
-    elif isinstance(v, Compare):
-        sa = create_js_statement(v.a)
-        sb = create_js_statement(v.b)
-        _op = {
-            'and': '&&', 'or': '||',
-            'eq': '==',  'ne': '!=',
-            'lt': '<',   'le': '<=',
-            'gt': '>',   'ge': '>='
-        }
-        return "(%s) %s (%s)" % (sa, _op[v.op], sb)
-    elif isinstance(v, bool):
-        if v: return "true"
-        else: return "false"
-    else:
-        return v
-
 class JavascriptHelper:
     def __init__(self, survey, container_id='survey', question_id_prefix="q_"):
         self.survey = survey
