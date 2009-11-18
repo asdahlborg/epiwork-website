c8a2e4a8f47bad5f8a927d35aed2be6f89d333c1
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-18 11:22:35 +0100

    Fix typo.

--- src/epiweb/apps/survey/urls.py
+++ src/epiweb/apps/survey/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^profile/$', 'epiwbe.apps.survey.profile_views.index'),
+    (r'^profile/$', 'epiweb.apps.survey.profile_views.index'),
     (r'^thanks/$', 'epiweb.apps.survey.survey_views.thanks'),
     (r'^$', 'epiweb.apps.survey.survey_views.index'),
 )
