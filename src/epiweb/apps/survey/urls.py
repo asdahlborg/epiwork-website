911c1d52307d98b55d0db51205fd6c4a3a8c42c3
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-18 11:25:59 +0100

    Rename survey_views to views.

--- src/epiweb/apps/survey/urls.py
+++ src/epiweb/apps/survey/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^profile/$', 'epiweb.apps.survey.survey_views.profile_index'),
-    (r'^thanks/$', 'epiweb.apps.survey.survey_views.thanks'),
-    (r'^$', 'epiweb.apps.survey.survey_views.index'),
+    (r'^profile/$', 'epiweb.apps.survey.views.profile_index'),
+    (r'^thanks/$', 'epiweb.apps.survey.views.thanks'),
+    (r'^$', 'epiweb.apps.survey.views.index'),
 )
 
