1c1d7f3b28334962b7498cc43aa30f127f372d65
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-11-26 10:03:00 +0100

    Copy password_* templates from django.contrib.auth's template. Remove the password_ prefix and put the inside a new directory so the will be used instead of the django's.

--- src/epiweb/apps/accounts/urls.py
+++ src/epiweb/apps/accounts/urls.py
@@ -1,6 +1,32 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
+    url(r'^password/change/$',
+        auth_views.password_change,
+        {'template_name': 'registration/password/change_form.html'},
+        name='auth_password_change'),
+    url(r'^password/change/done/$',
+        auth_views.password_change_done,
+        {'template_name': 'registration/password/change_done.html'},
+        name='auth_password_change_done'),
+    url(r'^password/reset/$',
+        auth_views.password_reset,
+        {'template_name': 'registration/password/reset_form.html',
+         'email_template_name': 'registration/password/reset_email.html'},
+        name='auth_password_reset'),
+    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'registration/password/reset_confirm.html'},
+        name='auth_password_reset_confirm'),
+    url(r'^password/reset/complete/$',
+        auth_views.password_reset_complete,
+        {'template_name': 'registration/password/reset_complete.html'},
+        name='auth_password_reset_complete'),
+    url(r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        {'template_name': 'registration/password/reset_done.html'},
+        name='auth_password_reset_done'),
     (r'', include('registration.backends.default.urls')),
 )
 
