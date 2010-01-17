3e8dcd75315f3ad1f2d20be52acaf0ae395c44fd
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-01-17 16:05:07 +0100

    Update accounts' urls.py

--- src/epiweb/apps/accounts/urls.py
+++ src/epiweb/apps/accounts/urls.py
@@ -1,7 +1,48 @@
 from django.conf.urls.defaults import *
+from epiweb.apps.accounts import views
 from django.contrib.auth import views as auth_views
+from django.views.generic.simple import direct_to_template
+
+from registration.views import activate
+from registration.views import register
 
 urlpatterns = patterns('',
+    # From registration.backends.default.urls
+    url(r'^activate/complete/$',
+        direct_to_template,
+        { 'template': 'registration/activation_complete.html' },
+        name='registration_activation_complete'),
+    # Activation keys get matched by \w+ instead of the more specific
+    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+    # that way it can return a sensible "invalid key" message instead of a
+    # confusing 404.
+    url(r'^activate/(?P<activation_key>\w+)/$',
+        activate,
+        { 'backend': 'registration.backends.default.DefaultBackend',
+          'template_name': 'registration/activate.html' },
+        name='registration_activate'),
+    url(r'^register/$',
+        register,
+        { 'backend': 'registration.backends.default.DefaultBackend',
+          'template_name': 'registration/registration_form.html' },
+        name='registration_register'),
+    url(r'^register/complete/$',
+        direct_to_template,
+        { 'template': 'registration/registration_complete.html' },
+        name='registration_complete'),
+    url(r'^register/closed/$',
+        direct_to_template,
+        { 'template': 'registration/registration_closed.html' },
+        name='registration_disallowed'),
+    # From registration.auth_urls
+    url(r'^login/$',
+        auth_views.login,
+        {'template_name': 'registration/login.html'},
+        name='auth_login'),
+    url(r'^logout/$',
+        auth_views.logout,
+        {'template_name': 'registration/logout.html'},
+        name='auth_logout'),
     url(r'^password/change/$',
         auth_views.password_change,
         {'template_name': 'registration/password_change_form.html'},
@@ -27,6 +68,8 @@ urlpatterns = patterns('',
         auth_views.password_reset_done,
         {'template_name': 'registration/password_reset_done.html'},
         name='auth_password_reset_done'),
-    (r'', include('registration.backends.default.urls')),
+
+    # Additional URLs
+    url(r'^$', views.index),
 )
 
