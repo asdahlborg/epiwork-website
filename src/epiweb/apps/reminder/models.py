beac0323253454f343b32d42d8c065cfc4fcc04f
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-01-18 18:41:40 +0100

    Set available options for weekday field of reminder's model

--- src/epiweb/apps/reminder/models.py
+++ src/epiweb/apps/reminder/models.py
@@ -4,11 +4,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+_ = lambda x: x
+
+# Reference: http://docs.python.org/library/time.html
+# - tm_wday => range [0,6], Monday is 0
+MONDAY = 0
+TUESDAY = 1
+WEDNESDAY = 2
+THURSDAY = 3
+FRIDAY = 4
+SATURDAY = 5
+SUNDAY = 6
+
+DAYS = (
+    (MONDAY, _('Monday')),
+    (TUESDAY, _('Tuesday')),
+    (WEDNESDAY, _('Wednesday')),
+    (THURSDAY, _('Thursday')),
+    (FRIDAY, _('Friday')),
+    (SATURDAY, _('Saturday')),
+    (SUNDAY, _('Sunday'))
+)
+
 class Reminder(models.Model):
     user = models.ForeignKey(User, unique=True)
     last_reminder = models.DateTimeField()
     next_reminder = models.DateField()
-    wday = models.IntegerField()
+    wday = models.IntegerField(choices=DAYS, verbose_name="Day",
+                               default=MONDAY)
     active = models.BooleanField()
 
 def add_reminder(sender, **kwargs):
