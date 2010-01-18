bd78472c14ce9ed487a563a958082b356e0b7c79
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-01-18 18:57:59 +0100

    Add actions to make reminders active or inactive

--- src/epiweb/apps/reminder/admin.py
+++ src/epiweb/apps/reminder/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from epiweb.apps.reminder.models import Reminder
 
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+make_active.short_description = 'Make selected reminders active'
+
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(active=False)
+make_inactive.short_description = 'Make selected reminders inactive'
+
 class ReminderAdmin(admin.ModelAdmin):
     list_display = ('user', 'wday', 'active', 
                     'last_reminder', 'next_reminder')
+    ordering = ('user__username',)
+    actions = (make_active, make_inactive,)
 
 admin.site.register(Reminder, ReminderAdmin)
 
