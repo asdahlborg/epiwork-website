3940fbc2caac4355757a9caf5f366644d1f50ccd
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-03-06 13:43:31 +0100

    Survey spec registration parses the input spec to validate it.

--- src/epiweb/apps/survey/management/commands/surveyregister.py
+++ src/epiweb/apps/survey/management/commands/surveyregister.py
@@ -3,7 +3,7 @@ from django.core.management.base import CommandError, BaseCommand
 
 class Command(BaseCommand):
     help = 'Register a survey specification.'
-    args = '<survey_id> <survey_specification_file>'
+    args = '<survey_specification_file>'
     option_list = BaseCommand.option_list + (
         make_option('--replace', action='store_true',
                     dest='replace', default=False,
@@ -12,32 +12,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            survey_id, fname = args
+            fname = args[0]
         except ValueError:
-            raise CommandError('Please enter survey_id and survey_specification_file.')
+            raise CommandError('Please enter survey_specification_file.')
 
-        from epiweb.apps.survey import models
+        import codecs
+        from epiweb.apps.survey import models, parser
+
         verbosity = int(options.get('verbosity', 1))
         replace = options.get('replace', False)
 
         try:
-            content = open(fname).read()
-        except IOError:
-            raise CommandError('Error while reading %s' % fname)
+            content = codecs.open(fname, 'r', 'utf-8').read()
+        except IOError, e:
+            raise CommandError('Error while reading %s: %s' % (fname, e))
 
-        items = models.Survey.objects.filter(survey_id=survey_id)
-        if len(items) > 0:
-            if replace:
-                for item in items:
-                    item.delete()
-            else:
-                raise CommandError('Duplicate survey_id: %s' % survey_id)
-
-        survey = models.Survey()
-        survey.survey_id = survey_id
-        survey.specification = content
-        survey.save()
+        try:
+            survey = parser.parse(content)
+        except Exception, e:
+            raise CommandError('Error while reading survey specification: %s' % e)
 
         if verbosity > 0:
-            print "A new survey specification registered: %s" % survey_id
+            print 'survey id: %s' % survey.id
+
+        items = models.Survey.objects.filter(survey_id=survey.id)
+        if len(items) > 0:
+            if replace:
+                data = items[0]
+            else:
+                raise CommandError('Duplicate survey_id: %s' % survey.id)
+        else:
+            data = models.Survey()
+
+        data.survey_id = survey.id
+        data.specification = content
+        data.save()
+
+        if verbosity > 0:
+            print 'A new survey specification registered.'
 
