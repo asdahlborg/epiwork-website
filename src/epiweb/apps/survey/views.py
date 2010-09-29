5d2fe7e3d934e2f9555936e8421d820f3cb30c6c
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-09-29 17:36:02 +0200

    Show user selection page before filling a survey or editing user profile

--- src/epiweb/apps/survey/views.py
+++ src/epiweb/apps/survey/views.py
@@ -2,7 +2,7 @@
 
 from django import forms
 from django.template import Context, loader, RequestContext
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.db import transaction
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
@@ -27,35 +27,42 @@ def get_active_survey_user(request):
         except models.SurveyUser.DoesNotExist:
             raise ValueError()
 
-def profile_required(func):
-    def redirect(request, *args, **kwargs):
-        url_profile = reverse('epiweb.apps.survey.views.profile_index')
-        url_survey = reverse('epiweb.apps.survey.views.index')
-        url = '%s?next=%s' % (url_profile, url_survey)
-        return HttpResponseRedirect(url)
-    def _func(request, *args, **kwargs):
-        survey_user = get_active_survey_user(request)
-        profile = utils.get_user_profile(survey_user)
-        if profile is None:
-            request.user.message_set.create(
-                message=_('You have to fill your profile data first.'))
-            return redirect(request)
-        else:
-            return func(request, *args, **kwargs)
-    return _func
-        
-
 @login_required
 def thanks(request):
     return render_to_response('survey/thanks.html',
         context_instance=RequestContext(request))
 
+def show_select_user(request, next, template='survey/select_user.html'):
+    users = models.SurveyUser.objects.filter(user=request.user)
+    if len(users) == 1:
+        survey_user = users[0]
+        url = '%s?gid=%s' % (next, survey_user.global_id)
+        return HttpResponseRedirect(url)
+
+    return render_to_response(template, {
+        'users': users,
+        'next': next,
+    }, context_instance=RequestContext(request))
+
 @login_required
-@profile_required
 def index(request):
     survey = utils.get_survey()
     form_class = utils.get_form_class(survey.id)
-    survey_user = get_active_survey_user(request)
+    try:
+        survey_user = get_active_survey_user(request)
+    except ValueError:
+        raise Http404()
+    if survey_user is None:
+        return show_select_user(request, reverse(index))
+
+    # Check if the user has filled user profile
+    if utils.get_user_profile(survey_user) is None:
+        request.user.message_set.create(
+            message=_('You have to fill your profile data first.'))
+        url = reverse('epiweb.apps.survey.views.profile_index')
+        url_next = reverse('epiweb.apps.survey.views.index')
+        url = '%s?gid=%s&next=%s' % (url, survey_user.global_id, url_next)
+        return HttpResponseRedirect(url)
 
     if request.method == 'POST':
         form = form_class(survey_user, request.POST)
@@ -88,7 +95,12 @@ def index(request):
 def profile_index(request):
     survey = utils.get_profile()
     form_class = utils.get_form_class(survey.id)
-    survey_user = get_active_survey_user(request)
+    try:
+        survey_user = get_active_survey_user(request)
+    except ValueError:
+        raise Http404()
+    if survey_user is None:
+        return show_select_user(request, reverse(profile_index))
 
     if request.method == 'POST':
         form = form_class(survey_user, request.POST)
@@ -102,6 +114,7 @@ def profile_index(request):
                 url = next
             else:
                 url = reverse('epiweb.apps.survey.views.profile_index')
+            url = '%s?gid=%s' % (url, survey_user.global_id)
 
             request.user.message_set.create(message=_('Profile saved.'))
             return HttpResponseRedirect(url)
