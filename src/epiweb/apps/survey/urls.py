366774525a95c5b7fe6c2b4893e35e503dd1d395
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-09-30 11:01:01 +0200

    Give name to survey's urls

--- src/epiweb/apps/survey/urls.py
+++ src/epiweb/apps/survey/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.defaults import *
 from . import views
 
 urlpatterns = patterns('',
-    (r'^profile/$', views.profile_index),
-    (r'^thanks/$', views.thanks),
+    url(r'^profile/$', views.profile_index, name='survey_profile'),
+    url(r'^thanks/$', views.thanks, name='survey_thanks'),
     url(r'^people/$', views.people, name='survey_people'),
     url(r'^people/add/$', views.people_add, name='survey_people_add'),
-    (r'^$', views.index),
+    url(r'^$', views.index, name='survey_index'),
 )
 
