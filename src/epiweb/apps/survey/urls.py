ec867f87441af657f0138ec723de4a52299284d8
Fajran Iman Rusadi <frusadi@science.uva.nl>
2010-09-30 11:00:19 +0200

    Use view's function instead of its name

--- src/epiweb/apps/survey/urls.py
+++ src/epiweb/apps/survey/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 
+from . import views
+
 urlpatterns = patterns('',
-    (r'^profile/$', 'epiweb.apps.survey.views.profile_index'),
-    (r'^thanks/$', 'epiweb.apps.survey.views.thanks'),
-    url(r'^people/$', 'epiweb.apps.survey.views.people', name='survey_people'),
-    url(r'^people/add/$', 'epiweb.apps.survey.views.people_add', name='survey_people_add'),
-    (r'^$', 'epiweb.apps.survey.views.index'),
+    (r'^profile/$', views.profile_index),
+    (r'^thanks/$', views.thanks),
+    url(r'^people/$', views.people, name='survey_people'),
+    url(r'^people/add/$', views.people_add, name='survey_people_add'),
+    (r'^$', views.index),
 )
 
