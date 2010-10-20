asdahlborg/epiwork-website

commit 8d1a5b4b070fa4fbd06a4ecd5743ef95a620ef23

Make select user page as a new separate page.
If no survey user is associated, opening the select user page
will redirect the user to survey user registration page.

src/epiweb/apps/survey/views.py
@@ -32,9 +32,18 @@ def thanks(request):
     return render_to_response('survey/thanks.html',
         context_instance=RequestContext(request))
 
-def show_select_user(request, next, template='survey/select_user.html'):
+@login_required
+def select_user(request, template='survey/select_user.html'):
+    next = request.GET.get('next', None)
+    if next is None:
+        next = reverse(index)
+
     users = models.SurveyUser.objects.filter(user=request.user)
-    if len(users) == 1:
+    total = len(users)
+    if total == 0:
+        url = '%s?next=%s' % (reverse(people_add), next)
+        return HttpResponseRedirect(url)
+    elif total == 1:
         survey_user = users[0]
         url = '%s?gid=%s' % (next, survey_user.global_id)
         return HttpResponseRedirect(url)
@@ -53,7 +62,8 @@ def index(request):
     except ValueError:
         raise Http404()
     if survey_user is None:
-        return show_select_user(request, reverse(index))
+        url = '%s?next=%s' % (reverse(select_user), reverse(index))
+        return HttpResponseRedirect(url)
 
     # Check if the user has filled user profile
     if utils.get_user_profile(survey_user) is None:
@@ -173,7 +183,12 @@ def people_add(request):
             request.user.message_set.create(
                 message=_('A new person has been added.'))
 
-            return HttpResponseRedirect(reverse(people))
+            next = request.GET.get('next', None)
+            if next is None:
+                url = reverse(people)
+            else:
+                url = '%s?gid=%s' % (next, survey_user.global_id)
+            return HttpResponseRedirect(url)
 
     else:
         form = forms.AddPeople()

src/epiweb/apps/survey/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^people/$', views.people, name='survey_people'),
     url(r'^people/add/$', views.people_add, name='survey_people_add'),
     url(r'^people/edit/$', views.people_edit, name='survey_people_edit'),
+    url(r'^select/$', views.select_user, name='survey_select_user'),
     url(r'^$', views.index, name='survey_index'),
 )
 

commit c079461911b8d49d6e0e9341e24de3bf8967f9ea

Change show_select_user calls with redirection to the select user page.
This bug was introduced by commit:8d1a5b4b that has incomplete change
to the usage of show_select_user. This commit closes #118.

src/epiweb/apps/survey/views.py
@@ -110,7 +110,8 @@ def profile_index(request):
     except ValueError:
         raise Http404()
     if survey_user is None:
-        return show_select_user(request, reverse(profile_index))
+        url = '%s?next=%s' % (reverse(select_user), reverse(profile_index))
+        return HttpResponseRedirect(url)
 
     if request.method == 'POST':
         form = form_class(survey_user, request.POST)
@@ -154,7 +155,8 @@ def people_edit(request):
     except ValueError:
         raise Http404()
     if survey_user is None:
-        return show_select_user(request, reverse(people_edit))
+        url = '%s?next=%s' % (reverse(select_user), reverse(people_edit))
+        return HttpResponseRedirect(url)
 
     if request.method == 'POST':
         form = forms.AddPeople(request.POST)