dd2997787f6717eaeee80463dcb6a8b3fc478f1e
Klaas van Schelven <klaas@vanschelven.com>
2011-07-16 15:46:48 +0200

    Settings reflect Django 1.3 deprecation

--- src/epiweb/settings.py
+++ src/epiweb/settings.py
@@ -73,8 +73,8 @@ SECRET_KEY = 'swgm*3%po62mg76m4iq!k8h3j+_)x=8b--7skjc_0wiak^wksr'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #    'django.template.loaders.eggs.load_template_source',
 )
 
