ac9e6ddf4efb31fee70cfc3b8cdb08676d6e637a
Klaas van Schelven <klaas@vanschelven.com>
2011-07-17 11:44:10 +0200

    Delete_all_objects: fixed again

--- apps/ew_contact_form/management/commands/delete_all_objects.py
+++ apps/ew_contact_form/management/commands/delete_all_objects.py
@@ -1,23 +1,29 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import get_models
+from django.db import transaction
 
+@transaction.commit_manually
 class Command(BaseCommand):
     help = 'Deletes all objects'
 
     def handle(self, *args, **options):
-        failed_somewhere = False
+        failed_somewhere = True
         max_tries = 100
 
-        while not failed_somewhere and max_tries > 0:
+        while failed_somewhere and max_tries > 0:
             # some deletions may trigger other deletions and fail; we'll just try many times
+            failed_somewhere = False
 
             max_tries -= 1
 
             for model in get_models():
                 try:
                     model.objects.all().delete()
-                except:
+                except Exception, e:
+                    print e
                     failed_somewhere = True
 
+        transaction.commit()
         if max_tries == 0:
             raise Exception("Giving up after 100 tries")
+
