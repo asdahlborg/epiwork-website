efe70a9409e999a4b7c8fe1d6ece80cfb2a25582
Antwan Wiersma <antwan@prime-creation.com>
2011-07-18 09:15:15 +0200

    Contactform offline for now

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ggm.db',             # Or path to database file if using sqlite3.
+        'NAME': 'influenzanet.db',             # Or path to database file if using sqlite3.
         'USER': '',             # Not used with sqlite3.
         'PASSWORD': '',         # Not used with sqlite3.
         'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
