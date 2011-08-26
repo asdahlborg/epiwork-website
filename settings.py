6ba1331290f5d65df3ade717cf904ccfd2e5e3a7
Klaas van Schelven <klaas@vanschelven.com>
2011-08-26 10:46:58 +0200

    Antwan's custom file icons are used

--- settings.py
+++ settings.py
@@ -75,6 +75,9 @@ MEDIA_URL = '/media/'
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin/media/'
 
+CMS_FILE_ICON_PATH = os.path.join(MEDIA_ROOT, 'file_icons/')
+CMS_FILE_ICON_URL = os.path.join(MEDIA_URL, 'file_icons/')
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'swgm*3%po62mg76m4iq!k8h3j+_)x=8b--7skjc_0wiak^wksr'
 
