7c646ad1963c17802c858d2f5f887a4819ceb0c7
Klaas van Schelven <klaas@vanschelven.com>
2011-08-27 12:51:02 +0200

    Fix on footer check-for-empty

--- apps/partnersites/context_processors.py
+++ apps/partnersites/context_processors.py
@@ -12,5 +12,5 @@ def customizations(request):
     return {
         'site_name': site.name,
         'site_logo': settings.logo.url if settings.logo else "",
-        'site_footer': mark_safe(clean_html(settings.footer, full=False)),
+        'site_footer': mark_safe(clean_html(settings.footer, full=False)) if settings.footer else None,
     }
