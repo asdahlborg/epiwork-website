b038ba0a0e4908dc7a4f8354952a871aa1c1d6a4
Antwan Wiersma <antwan@prime-creation.com>
2011-08-31 11:16:48 +0200

    Removed obsolete Analytics code from influenzanet.js

--- media/base/js/influenzanet.js
+++ media/base/js/influenzanet.js
@@ -5,18 +5,6 @@ Author: Antwan Wiersma (http://www.prime-creation.nl/)
 Version: 1.0
 */
 
-/* Google Analytics */
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-24124829-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 /* Influenzanet Map Bar */
 
 var add_link = function() {
