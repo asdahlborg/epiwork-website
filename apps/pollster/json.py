7974ba3fee69a74b1ec3612b22b4526f0f31b149
Emanuele Aina <emanuele.aina@dndg.it>
2011-09-19 23:55:31 +0200

    JSON encode datetime.date as text

--- apps/pollster/json.py
+++ apps/pollster/json.py
@@ -5,6 +5,8 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return obj.strftime('%Y-%m-%dT%H:%M:%S')
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
         else:
             return json.JSONEncoder.default(self, obj)
 
