a01a5a4c7ba4ddfa06458ad77300ee8a6e38c7d6
Klaas van Schelven <klaas@vanschelven.com>
2011-09-26 12:45:01 +0200

    Added a submenu for 'survey'; included some fake data to make Surveys testable in Debug

---
+++ apps/survey/menu.py
@@ -0,0 +1,33 @@
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+from cms.models import Page
+from cms.menu_bases import CMSAttachMenu
+from menus.base import Menu, NavigationNode
+from menus.menu_pool import menu_pool
+
+from . import views
+
+class SurveyMenu(CMSAttachMenu):
+    name = "Survey menu"
+
+    def get_nodes(self, request):
+        """
+        This method is used to build the menu tree.
+        """
+        nodes = [
+            NavigationNode(
+                _("Weekly survey"),
+                reverse(views.index),
+                "views.index",
+            ),
+            NavigationNode(
+                _("Manage others"),
+                reverse(views.people),
+                "views.people",
+            ),
+        ]
+
+        return nodes
+
+menu_pool.register_menu(SurveyMenu)
