62f64027351ca4acb70755078c9fc3c742f7b56a
Klaas van Schelven <klaas@vanschelven.com>
2011-09-27 21:38:31 +0200

    Import script for GGM

---
+++ apps/accounts/management/commands/import_ggm.py
@@ -0,0 +1,51 @@
+import MySQLdb
+
+from django.core.management.base import NoArgsCommand, BaseCommand 
+from django.db import transaction
+from django.contrib.auth.models import User
+
+from apps.reminder.models import UserReminderInfo
+from apps.survey.models import SurveyUser
+
+class Command(NoArgsCommand):
+
+    @transaction.commit_on_success
+    def handle_noargs(self, **options):
+        c = MySQLdb.connect(host="localhost", user="root", passwd="", db="ggm_existing_tmp")
+        cursor = c.cursor ()
+        cursor.execute ("SELECT * FROM `meter`")
+
+        rows = cursor.fetchall()
+        for row in rows:
+            meter_id, UUID, email, naam, postcode, geb_datum, geslacht, password, laatste_meting, laatste_mail, herinnering, reken_postcode, wil_herinnering, stress = row
+
+            u = User(
+                email=email,
+                username=email,
+            )
+            u.set_password(password)
+            u.save()
+
+            reminder = UserReminderInfo.objects.create(
+                user=u,
+                active=(wil_herinnering == 1),
+                language='nl',
+            )
+
+            survey_user = SurveyUser.objects.create(
+                user=u,
+                global_id=UUID,
+                name=naam,
+                last_participation_date=laatste_meting,
+            )
+
+            naam # may be put in the SurveyUser
+            
+            # postcode: somewhere in 'intake'?
+            # reken_postcode: same same
+            # geb_datum, geslacht: same same
+            # laatste_mail is unusable
+
+        cursor.close ()
+        c.close ()
+
