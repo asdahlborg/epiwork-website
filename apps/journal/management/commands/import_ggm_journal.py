78f0e3e4d5085996263c2f680f4c2ebdaf9e847c
Klaas van Schelven <klaas@vanschelven.com>
2011-10-09 15:38:15 +0200

    Management command for importing news

---
+++ apps/journal/management/commands/import_ggm_journal.py
@@ -0,0 +1,46 @@
+import MySQLdb
+
+from django.core.management.base import NoArgsCommand, BaseCommand 
+from django.db import transaction
+from django.template.defaultfilters import slugify
+
+from apps.journal.models import Entry
+
+class Command(NoArgsCommand):
+
+    @transaction.commit_on_success
+    def handle_noargs(self, **options):
+        c = MySQLdb.connect(host="localhost", user="root", passwd="", db="tmp_ggm_news", charset='utf8')
+        cursor = c.cursor ()
+        cursor.execute ("SELECT * FROM `articles_filed`")
+
+        rows = cursor.fetchall()
+        for i, row in enumerate(rows):
+            id, art_id, date, section_name, title, html, abstract = row
+
+            entry = Entry.objects.language('nl').create(
+                slug=slugify(title),
+                image=None,
+                alignment=None,
+                is_published=True,
+                pub_date=date,
+                created=date,
+                updated=date,
+                category=None,
+
+                title=title,
+                excerpt=abstract,
+                content=html,
+            )
+
+            
+            # Next: translate
+            #translations = TranslatedFields(
+                #title           = models.CharField(_('Title'), max_length=255),
+                #excerpt         = models.TextField(_('Excerpt'), blank=True),
+                #content         = models.TextField(_('Content'), blank=True),
+            #)
+
+        cursor.close ()
+        c.close ()
+
