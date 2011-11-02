f7ac628a40b26b65b1a5f9785082d53706da55c9
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-11-02 17:25:15 +0000

    UK regexp

--- apps/pollster/fields.py
+++ apps/pollster/fields.py
@@ -12,6 +12,7 @@ YEARMONTH_INPUT_FORMATS = (
 
 POSTALCODE_INPUT_FORMATS = {
     'it': r'\d{5}', # e.g. 10100
+    'uk': r'(GIR)|(((A[BL]|B[ABDHLNRSTX]?|C[ABFHMORTVW]|D[ADEGHLNTY]|E[HNX]?|F[KY]|G[LUY]?|H[ADGPRSUX]|I[GMPV]|JE|K[ATWY]|L[ADELNSU]?|M[EKL]?|N[EGNPRW]?|O[LX]|P[AEHLOR]|R[GHM]|S[AEGKLMNOPRSTY]?|T[ADFNQRSW]|UB|W[ADFNRSV]|YO|ZE)[1-9]?[0-9]|((E|N|NW|SE|SW|W)1|EC[1-4]|WC[12])[A-HJKMNPR-Y]|(SW|W)([2-9]|[1-9][0-9])|EC[1-9][0-9]))', #
 }
 
 logger = logging.getLogger(__name__)
